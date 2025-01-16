88ac5744f02d1a150360a8da5190414f51025b66
vicenteM94 <vmatheusbdl@gmail.com>
2025-01-16 21:16:33 +0000

    definicao de variaveis e exibicao das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,32 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    char codigo[4];
+    char nome[50];
+    float populacao;
+    float area;
+    float pib;
+    int pontosTuristicos;
+
+    printf("Digite o código da cidade (ex: A01): \n");
+    scanf("%s", codigo);
+    printf("Digite o nome da cidade: \n");
+    scanf(" %[^\n]", nome);
+    printf("Digite a população da cidade: \n");
+    scanf("%f", &populacao);
+    printf("Digite a área da cidade: \n");
+    scanf("%f", &area);
+    printf("Digite o PIB da cidade: \n");
+    scanf("%f", &pib);
+    printf("Digite os pontos turísticos da cidade: \n");
+    scanf("%d", &pontosTuristicos);
+
+    printf("Código: %s\n", codigo);
+    printf("Nome: %s\n", nome);
+    printf("População: %.2f\n", populacao);
+    printf("Área: %.2f\n", area);
+    printf("PIB: %.2f\n", pib);
+    printf("Pontos Turísticos: %d\n", pontosTuristicos);
+
     return 0;
 }
